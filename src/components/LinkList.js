d01cd6e9dce236e0d3d037b5e12b0f33ae93db26
Heather Park <hthr.prk@gmail.com>
2018-05-24 10:43:45 -0700

    Remove mock data from LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,18 +6,15 @@ import gql from 'graphql-tag';
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com'
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/'
-      }
-    ];
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.feedQuery.feed.links;
 
     return (
       <div>{linksToRender.map(link => <Link key ={link.id} link={link} />)}</div>
